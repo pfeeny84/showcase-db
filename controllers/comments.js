bc5a4cc272ae74857aa905cc240c924bca083d54
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-03-16 18:15:07 -0700

    pushing before making changes

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,11 +7,11 @@ const Showcase = require('../models/showcase');
  };
 
  function create(req, res) {
-     console.log('button clicked')
+    //  console.log('button clicked')
      Showcase.findById(req.params.id, function(err, showcase) {
-        console.log(showcase,'this is showcase')
+        // console.log(showcase,'this is showcase')
         // req.body.user = req.user._id;
-        console.log(req.body, 'this is req.body');
+        // console.log(req.body, 'this is req.body');
         showcase.comments.push(req.body);
         showcase.save(function(err) {
         res.redirect(`/showcases/${showcase._id}`);
