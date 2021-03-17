667d9c66946f96b95edf457caa93b6693bb5eb82
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-03-16 19:05:51 -0700

    added update comment but need to figure out why its not saving

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,7 +3,8 @@ const Showcase = require('../models/showcase');
  
  module.exports = {
     create,
-    delete: deleteComment
+    delete: deleteComment,
+    update,
  };
 
  function create(req, res) {
@@ -28,3 +29,21 @@ function deleteComment(req, res) {
         });
     });
 }
+
+function update(req, res) {
+    // Note the cool "dot" syntax to query on the property of a subdoc
+    Showcase.findOne({'comments._id': req.params.id}, function(err, showcase) {
+      // Find the comment subdoc using the id method on Mongoose arrays
+      
+      const commentSubdoc = showcase.comments.id(req.params.id);
+      // Ensure that the comment was created by the logged in user
+    //   if (!commentSubdoc.user.equals(req.user._id)) return res.redirect(`/showcases/${showcase._id}`);
+      // Update the text of the comment
+      commentSubdoc.text = req.body.text;
+      // Save the updated book
+      showcase.save(function(err) {
+        // Redirect back to the book's show view
+        res.redirect(`/showcases/${showcase._id}`);
+      });
+    });
+  }
--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,6 @@ const commentsCtrl = require('../controllers/comments');
 
 router.post('/showcases/:id/comments', commentsCtrl.create);
 router.delete('/comments/:id', commentsCtrl.delete);
+router.put('/comments/:id', commentsCtrl.update);
 
 module.exports = router;
