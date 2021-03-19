be15414f019e254b14d4894bd830a155943dfeef
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-03-18 18:37:21 -0700

    code cleanup

--- controllers/comments.js
+++ controllers/comments.js
@@ -29,22 +29,13 @@ function deleteComment(req, res) {
 }
 
 function update(req, res) {
-    // Note the cool "dot" syntax to query on the property of a subdoc
-    Showcase.findOne({'comments._id': req.params.id}, function(err, showcase) {
-      // Find the comment subdoc using the id method on Mongoose arrays
-      
-      const commentSubdoc = showcase.comments.id(req.params.id);
-      // Ensure that the comment was created by the logged in user
-      
-      if (!commentSubdoc.user.equals(req.user._id)) return res.redirect(`/showcases/${showcase._id}`);
     
-      // Update the text of the comment
-      commentSubdoc.content = req.body.text;
-      console.log(commentSubdoc);
-      // Save the updated book
-      showcase.save(function(err) {
-        // Redirect back to the book's show view
-        res.redirect(`/showcases/${showcase._id}`);
-      });
+  Showcase.findOne({'comments._id': req.params.id}, function(err, showcase) {
+    const commentSubdoc = showcase.comments.id(req.params.id);
+    if (!commentSubdoc.user.equals(req.user._id)) return res.redirect(`/showcases/${showcase._id}`);
+    commentSubdoc.content = req.body.text;
+    showcase.save(function(err) {
+      res.redirect(`/showcases/${showcase._id}`);
     });
-  }
+  });
+}
